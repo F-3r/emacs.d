75f8d7223fe2e0530fe57963bfdc016ccd3b05fd
F-3r <f.fernando.martinez@gmail.com>
2013-05-08 11:57:40 -0300

    added zenburn color theme

--- init.el
+++ init.el
@@ -25,6 +25,9 @@
     (package-refresh-contents)
     (package-install package)))
 
+;;enable color theme
+(load-theme 'zenburn t)
+
 ;; Remove clutter
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
