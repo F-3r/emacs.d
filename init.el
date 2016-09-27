53bf4e54e50c0743aeda955af83ab48da7263b96
Fernando Martinez <fernando.martinez@live.com.ar>
2016-09-27 09:40:56 -0300

    sc

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/config")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/SuperCollider")
 
 (load-library "config-packages")
 (load-library "config-global")
 (load-library "config-ruby.el")
 (load-library "config-web.el")
 (load-library "config-key-bindings.el")
+
+(require 'sclang)
